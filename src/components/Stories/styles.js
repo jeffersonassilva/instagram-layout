cd2094134753953da634aabdf657f652e4914809
Jefferson Silva <jeffersonassilva@gmail.com>
2019-04-27 18:46:19 -0300

    padronizando os imports

--- src/components/Stories/styles.js
+++ src/components/Stories/styles.js
@@ -1,6 +1,5 @@
 import {StyleSheet} from 'react-native';
-import {metrics, colors} from '../../styles';
-import fonts from "../../styles/fonts";
+import {fonts, colors, metrics} from '../../styles';
 
 const styles = StyleSheet.create({
     container: {
