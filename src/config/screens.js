87b63aa01d74379ae55a4b54f921012bd92881fd
Jefferson Silva <jeffersonassilva@gmail.com>
2019-05-05 15:48:36 -0300

    controlando melhor as telas

---
+++ src/config/screens.js
@@ -0,0 +1,15 @@
+import ActivitiesScreen from "../screens/Activities";
+import CameraScreen from "../screens/Camera";
+import MeScreen from "../screens/Me";
+import HomeScreen from "../screens/Home";
+import ProfileScreen from "../screens/Profile";
+import SearchScreen from "../screens/Search";
+
+const Activities = ActivitiesScreen;
+const Camera = CameraScreen;
+const Home = HomeScreen;
+const Me = MeScreen;
+const Profile = ProfileScreen;
+const Search = SearchScreen;
+
+export {Activities, Camera, Home, Me, Search, Profile};
